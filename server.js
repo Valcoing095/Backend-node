29189c095012b16fac84d77ce9715185823ee208
Yeison <yvelasco321@gmail.com>
2021-04-20 20:49:44 -0500

    Obteniendo el body por consola

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
 
 var app = express();
+app.use(router);
+app.use(express.json());
 
-app.use('/',function(req,resp){
-    resp.send('Hola');
-});
+app.get('/message',function(req,res){
+    res.send('Lista de mensjaes');
+})
+app.delete('/message',function(req,res){
+    console.log(req.body)
+    res.send('Mensaje añadido')
+})
 
 app.listen(3080);
