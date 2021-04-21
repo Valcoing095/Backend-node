181bc9c64fb947617a52c682f0890dbeea65af52
Yeison <yvelasco321@gmail.com>
2021-04-21 17:17:58 -0500

    Response

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
-
+const response  =require('./networks/response')
 var app = express();
 app.use(router);
 app.use(express.json());
 
+
+
+
 app.get('/message',function(req,res){
-    res.send('Lista de mensjaes');
+    console.log(req.headers)
+    res.header({
+        "custom-header":"Nuestro valor personalizado"
+    })
+    // res.send('Lista de mensjaes');
+
+    response.success(req,res,'Lista de mensajes')
 })
-app.delete('/message',function(req,res){
-    console.log(req.body)
-    res.send('Mensaje añadido')
-})
+
+
+app.post('/message',function(req,res){
+    console.log(req.query);
+    if(req.query.error == "ok"){
+        response.error(req,res,"error inespera",500, "Es una simulasion de los errores")
+    }else{
+
+        response.success(req,res,'Creado correctamente',201)
+    }
+    // res.status(201).send({err:'Ha ocurrido un error', message:'Creado Correctamente'})
+});
+
+app.use('/app', express.static('public'));
 
 app.listen(3080);
